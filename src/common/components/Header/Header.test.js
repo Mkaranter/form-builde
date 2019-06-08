c2d64d0ae81a17576140bce69f20e15be72efe24
Michał Karanter <michal.karanter@gmail.comgit config --global user.name Micha>
2019-06-08 13:36:51 +0200

    FormBuilder should have 2 buttons

--- src/common/components/Header/Header.test.js
+++ src/common/components/Header/Header.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Header from './Header'
 
-it('Header renders without crashing', () => {
+it('Render header', () => {
     const component = shallow(<Header />)
     const header = <h1>FORM BUILDER APP</h1>
     expect(component.contains(header)).toEqual(true)
---
+++ src/views/FormBuilder/FormBuilder.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import FormBuilder from './FormBuilder'
+import Button from 'common/components/Button'
+
+test('Form detail should contain styled button', () => {
+    const component = shallow(<FormBuilder />)
+
+    expect(component.find(Button).exists()).toBe(true)
+})
+
+test('Form detail should contain 2 styled button', () => {
+    const component = shallow(<FormBuilder />)
+    const button = component.find(Button)
+
+    expect(button).toHaveLength(2)
+})

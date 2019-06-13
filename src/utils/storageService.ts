544e8858779bbb9fe645cea15cbdce22915355b9
Michał Karanter <michal.karanter@skygate.io>
2019-06-13 14:17:15 +0200

    storage service as factory

--- src/utils/storageService.ts
+++ src/utils/storageService.ts
@@ -1,30 +1,68 @@
 import { openDb } from 'idb'
 import { Question } from 'common/models'
 
-const dbPromise = openDb('form-db', 1, upgradeDB => {
-    upgradeDB.createObjectStore('formStore', {
-        keyPath: 'id',
-        autoIncrement: true,
-    })
-})
+// export const storageService = {
+//     async getAllQuestions() {
+//         const db = await dbPromise
+//         return db
+//             .transaction('formStore')
+//             .objectStore('formStore')
+//             .getAll()
+//             .then(res => res)
+//     },
 
-export const storageService = {
-    async getAllQuestions() {
+//     async updateQuestion(updatedQuestion: Question) {
+//         const db = await dbPromise
+//         const tx = db.transaction('formStore', 'readwrite')
+//         return tx.objectStore('formStore').put(updatedQuestion)
+//     },
+
+//     async addQuestion(newQuestion: Omit<Question, 'id'>) {
+//         if (newQuestion.children) delete newQuestion.children
+//         const db = await dbPromise
+//         const tx = db.transaction('formStore', 'readwrite')
+//         return tx
+//             .objectStore('formStore')
+//             .put(newQuestion)
+//             .then(res => res)
+//     },
+
+//     async deleteQuestion(key: number) {
+//         const db = await dbPromise
+//         const tx = db.transaction('formStore', 'readwrite')
+//         return tx
+//             .objectStore('formStore')
+//             .delete(key)
+//             .then(() => {
+//                 return key
+//             })
+//     },
+// }
+
+const StorageServiceFactory = () => {
+    const dbPromise = openDb('form-db', 1, upgradeDB => {
+        upgradeDB.createObjectStore('formStore', {
+            keyPath: 'id',
+            autoIncrement: true,
+        })
+    })
+
+    const getAllQuestions = async () => {
         const db = await dbPromise
         return db
             .transaction('formStore')
             .objectStore('formStore')
             .getAll()
             .then(res => res)
-    },
+    }
 
-    async updateQuestion(updatedQuestion: Question) {
+    const updateQuestion = async (updatedQuestion: Question) => {
         const db = await dbPromise
         const tx = db.transaction('formStore', 'readwrite')
         return tx.objectStore('formStore').put(updatedQuestion)
-    },
+    }
 
-    async addQuestion(newQuestion: Omit<Question, 'id'>) {
+    const addQuestion = async (newQuestion: Omit<Question, 'id'>) => {
         if (newQuestion.children) delete newQuestion.children
         const db = await dbPromise
         const tx = db.transaction('formStore', 'readwrite')
@@ -32,9 +70,9 @@ export const storageService = {
             .objectStore('formStore')
             .put(newQuestion)
             .then(res => res)
-    },
+    }
 
-    async deleteQuestion(key: number) {
+    const deleteQuestion = async (key: number) => {
         const db = await dbPromise
         const tx = db.transaction('formStore', 'readwrite')
         return tx
@@ -43,5 +81,14 @@ export const storageService = {
             .then(() => {
                 return key
             })
-    },
+    }
+
+    return Object.freeze({
+        getAllQuestions,
+        updateQuestion,
+        addQuestion,
+        deleteQuestion,
+    })
 }
+
+export const storageService = new (StorageServiceFactory as any)()

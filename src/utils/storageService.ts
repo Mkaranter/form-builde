9e6c20ceb9abca9a4b62690cd86625de8fa218b3
Michał Karanter <michal.karanter@skygate.io>
2019-06-13 14:17:32 +0200

    removed commented code

--- src/utils/storageService.ts
+++ src/utils/storageService.ts
@@ -1,44 +1,6 @@
 import { openDb } from 'idb'
 import { Question } from 'common/models'
 
-// export const storageService = {
-//     async getAllQuestions() {
-//         const db = await dbPromise
-//         return db
-//             .transaction('formStore')
-//             .objectStore('formStore')
-//             .getAll()
-//             .then(res => res)
-//     },
-
-//     async updateQuestion(updatedQuestion: Question) {
-//         const db = await dbPromise
-//         const tx = db.transaction('formStore', 'readwrite')
-//         return tx.objectStore('formStore').put(updatedQuestion)
-//     },
-
-//     async addQuestion(newQuestion: Omit<Question, 'id'>) {
-//         if (newQuestion.children) delete newQuestion.children
-//         const db = await dbPromise
-//         const tx = db.transaction('formStore', 'readwrite')
-//         return tx
-//             .objectStore('formStore')
-//             .put(newQuestion)
-//             .then(res => res)
-//     },
-
-//     async deleteQuestion(key: number) {
-//         const db = await dbPromise
-//         const tx = db.transaction('formStore', 'readwrite')
-//         return tx
-//             .objectStore('formStore')
-//             .delete(key)
-//             .then(() => {
-//                 return key
-//             })
-//     },
-// }
-
 const StorageServiceFactory = () => {
     const dbPromise = openDb('form-db', 1, upgradeDB => {
         upgradeDB.createObjectStore('formStore', {

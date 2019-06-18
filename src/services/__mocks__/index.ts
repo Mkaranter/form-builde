254d68a6288aa6ef52241478843c9bf3cd155dad
Michał Karanter <michal.karanter@gmail.comgit config --global user.name Micha>
2019-06-18 19:15:02 +0200

    clean up

--- src/services/__mocks__/index.ts
+++ src/services/__mocks__/index.ts
@@ -2,14 +2,8 @@ import { questions } from 'common/__mocks__/questions'
 
 const storageService = () => {
     const getAll = () => {
-        return new Promise((resolve, reject) => {
-            process.nextTick(() =>
-                true
-                    ? resolve(questions)
-                    : reject({
-                          error: 'Something went wrong',
-                      })
-            )
+        return new Promise(resolve => {
+            process.nextTick(() => resolve(questions))
         })
     }
 

68d61252820305712256d5bd4e99a8e95996c307
Michał Karanter <michal.karanter@gmail.comgit config --global user.name Micha>
2019-06-25 20:55:55 +0200

    simplify test

--- src/views/UserForm/__tests__/checkForCondition.test.ts
+++ src/views/UserForm/__tests__/checkForCondition.test.ts
@@ -1,11 +1,10 @@
 import { QuestionConditionTypes } from 'utils/enums'
 
 import { checkForCondition } from '../checkForCondition'
-import { randomStringsArray, randomLevel } from '../__mocks__/mocks'
 
 describe('should check condition', () => {
     it('should return true for level = 0 ', () => {
-        const result = checkForCondition('fweffw', 'wfwf', 0)
+        const result = checkForCondition('someString', 'anotherString', 0)
 
         expect(result).toBe(true)
     })
@@ -13,22 +12,22 @@ describe('should check condition', () => {
     it('should return true for type equals if values are equal ', () => {
         const result = checkForCondition(
             QuestionConditionTypes.Equals,
-            randomStringsArray[0], // just use hardcoded strings
-            randomLevel,
-            randomStringsArray[0]
+            'shortString',
+            3,
+            'longerString'
         )
 
         expect(result).toBe(true)
     })
 
     it('should return true for type less if form input is smaller than condition value', () => {
-        const result = checkForCondition(QuestionConditionTypes.Less, '34', randomLevel, '1')
+        const result = checkForCondition(QuestionConditionTypes.Less, '34', 4, '1')
 
         expect(result).toBe(true)
     })
 
     it('should return true for type greater if form inpu is greater than condition value', () => {
-        const result = checkForCondition(QuestionConditionTypes.Greater, '1', randomLevel, '11')
+        const result = checkForCondition(QuestionConditionTypes.Greater, '1', 3, '11')
 
         expect(result).toBe(true)
     })
